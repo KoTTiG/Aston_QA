c51cf17b694e204f810496d897af5af4060f7182
Dmitry Gasilov <d.gasilov@mail.ru>
2024-07-22 14:32:40 +0300

    task9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,62 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String[][] wrongSizeArray = { {"1", "1", "1", "1"},
+                {"2", "2", "2", "2"},
+                {"3", "3", "3", "3"} };
+        String[][] intArray = { {"1", "1", "1", "1"},
+                {"2", "2", "2", "2"},
+                {"3", "3", "3", "3"},
+                {"4", "4", "4", "4"}};
+        String[][] strArray = { {"1", "a1", "1", "1"},
+                {"2", "2", "2", "2"},
+                {"3", "3", "3", "3"},
+                {"4", "4", "4", "4"}};
+        try {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+            checkArraySize(intArray);
+            checkArraySize(wrongSizeArray);
+
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка: " + e.getMessage());
         }
+        finally {
+            try {
+
+
+                SumArray(intArray);
+                SumArray(strArray);
+
+            } catch (MyArrayDataException e) {
+                System.out.println("Ошибка: " + e.getMessage());
+            }
+        }
+
     }
+
+    public static void SumArray(String[][] array) throws MyArrayDataException {
+
+            int sum = 0;
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[0].length; j++) {
+                    try {
+                    sum = sum + Integer.valueOf(array[i][j]);
+                    }
+                    catch (NumberFormatException e) {
+                        throw new MyArrayDataException ("Неправильный формат данных в ячейке [" + i +";"+j+"]");
+                    }
+                }
+            }
+            System.out.println("Сумма элементов в массиве = " +sum);
+
+
+    }
+
+    public static void checkArraySize(String[][] array) throws MyArraySizeException {
+
+        if ((array.length != 4) || (array[0].length != 4)) throw new MyArraySizeException("Неправильный размер массива");
+    }
+
+
 }

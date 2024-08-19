0cade096047997918491e25c6029512d1d3a0430
Dmitry Gasilov <d.gasilov@mail.ru>
2024-08-19 20:49:00 +0300

    task11_junit_fix

--- src/main/java/org/example/Factorial.java
+++ src/main/java/org/example/Factorial.java
@@ -4,7 +4,7 @@ public class Factorial {
 
     public int factorial(int a) {
         int fact = 1;
-        if (a < 0) return -1;
+        if (a < 0) throw new ArithmeticException("Отрицательно число");
         for (int i = 1; i <= a; i++){
             fact = fact * i;
         }
--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,14 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.example.Factorial;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class FactorialTest {
 
 
     @Test
-    public void test1() {
+    public void testFactorialPositive() {
         Factorial factorial = new Factorial();
         int actual = factorial.factorial(5);
         int expected = 120;
@@ -17,7 +17,7 @@ class FactorialTest {
     }
 
     @Test
-    public void test2() {
+    public void testFactorialZero() {
         Factorial factorial = new Factorial();
         int actual = factorial.factorial(0);
         int expected = 1;
@@ -26,11 +26,10 @@ class FactorialTest {
     }
 
     @Test
-    public void test3() {
+    public void testFactorialNegative() {
         Factorial factorial = new Factorial();
-        int actual = factorial.factorial(-5);
-        int expected = -1;
-        assertEquals(actual, expected);
-
+        Assertions.assertThrows(ArithmeticException.class, () -> {
+            factorial.factorial(-5);
+        });
     }
 }
